918bfbd58a813bdcebf803a40fa3de90855d2deb
Rocco000 <r.iuliano13@studenti.unisa.it>
2024-11-09 20:08:24 +0100

    Rocco000/Crator#3 Updated extract_cookie script based on the previous changes

--- python/extract_cookie.py
+++ python/extract_cookie.py
@@ -1,10 +1,12 @@
 import re
 import socket
+import time
 
 # Local import
-from handler import TorCookiesHandler
+from creator import Creator
 from utils.config import Configuration
 from utils.seeds import get_seeds
+from handler import TorHandler
 
 def is_port_open(host, port):
     """
@@ -18,6 +20,33 @@ def is_port_open(host, port):
         sock.settimeout(1)  # Set a timeout for the connection attempt
         result = sock.connect_ex((host, port))
         return result == 0
+    
+def delay_based_on_cookie_extracted(seed: str, config: Configuration) -> None:
+    """
+    Delay the cookie extraction based on number of cookies stored in the YAML file.
+
+    :param seed: the URL to be crawled
+    :param config: a Configuration object which handles the YAML file
+    """
+    num_cookie_stored = len(config.cookies(seed))
+
+    delay = 0
+
+    if num_cookie_stored >= 50:
+        delay = (2 * 60) + 30
+    elif num_cookie_stored >= 40:
+        delay =  2 * 60 
+    elif num_cookie_stored >= 30:
+        delay =  90  
+    elif num_cookie_stored >= 20:
+        delay = 60
+    elif num_cookie_stored >= 10:
+        delay = 30
+    else:
+        delay = 0
+
+    print(f"Reached {num_cookie_stored} cookies, delaying for {delay / 60} minute(s)...")
+    time.sleep(delay)
 
 if __name__ == '__main__':
     config = Configuration()
@@ -27,26 +56,29 @@ if __name__ == '__main__':
     seed = None
     if len(seeds) == 1:
         seed = seeds[0]
+    else:
+        raise Exception("Define only one seed URL")
 
-    # Define a regular expression to extract the base onion URL, i.e. the homepage URL
-    pattern = rf"(.*?{re.escape('.onion/')})"
-    match = re.search(pattern, seed)
-    homepage_url = None
-    if match:
-        homepage_url = match.group(1)
-        print(homepage_url)
-    
-    tor_cookie_handler = TorCookiesHandler(config.tor_password(), config.second_tor_port(), config.second_tor_proxy(), config.venv_path(), config.cookie_waiting_time(), config.cookie_attempts(), homepage_url, seed)
+    scraper = Creator.create_scraper(config.marketplace(), config.macro_category(), config.micro_category())
+
+    # Get the homepage URL
+    homepage_url = scraper.get_base_url(seed)
+    print(f"Homepage URL: {homepage_url}")
+
+    tor_handler = TorHandler(config.tor_password(), config.second_tor_port(), config.second_tor_proxy(), config.venv_path())
+
+    # Get the correct instance of cookie extractor
+    cookie_extractor = Creator.create_cookie_extractor(tor_handler, homepage_url, seed, config.cookie_waiting_time(), config.cookie_attempts())
     
     # Get the Tor port used by the crawler
-    crawler_tor_port =config.tor_port()
+    crawler_tor_port = config.tor_port()
     n_cookies = 0
     counter = 1
 
     # Store cookies until the crawler doesn't stop
     while is_port_open("127.0.0.1", crawler_tor_port):
         print(f"Attempt {counter} to acquire a new cookie\n")
-        result = tor_cookie_handler.store_new_cookie()
+        result = cookie_extractor.store_new_cookie()
 
         counter += 1
         
@@ -54,6 +86,9 @@ if __name__ == '__main__':
             print("*************************\n")
             n_cookies += 1
             counter = 1
+
+        delay_based_on_cookie_extracted(seed, config)
+        
     
     print(f"Number of cookie extracted: {n_cookies}")
-    tor_cookie_handler.stop_tor_process()
+    cookie_extractor.stop()
