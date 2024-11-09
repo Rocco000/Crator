01e283eeb95b1d3581e5b2f90052521f8f73169b
Rocco000 <r.iuliano13@studenti.unisa.it>
2024-11-09 16:32:38 +0100

    Rocco000/Crator#1 Implemented two new abstract methods in the DrughubScraper class: - Added implementations for get_base_url and get_url_category
    Refined regular expressions:
    - Improved pattern matching for price extraction
    - Improved the category check

--- python/scrapers/drughub_scraper.py
+++ python/scrapers/drughub_scraper.py
@@ -1,5 +1,7 @@
 import re
 from bs4 import BeautifulSoup
+from bs4.element import Tag
+from urllib.parse import urljoin, urlparse
 
 # Local import
 from scrapers import Scraper
@@ -59,7 +61,7 @@ class DrughubScraper(Scraper):
                 product_information["destination"] = match.group(2)
 
             # Extract price information
-            match = re.search(r"Price \((\w+)\):\s*([\d.]+)", div_text)
+            match = re.search(r"Price \((\w+)\):\s*(\d+(\.\d+)?)", div_text)
 
             if match:
                 currency = match.group(1)
@@ -73,21 +75,42 @@ class DrughubScraper(Scraper):
                     product_information["crypto_price"] = price
         
         return product_information
+    
+    def get_base_url(self, web_page_url: str) -> str:
+        # Get all characters until .onion/
+        match = re.search(rf"(.*?{re.escape('.onion/')})", web_page_url)
+        
+        return match.group(1) if match else ""
+        
+    def get_url_category(self, url: str) -> str:
+        x = urlparse(url).path.split("/")
+        url_category = "/".join(x[-2:])
 
-    def check_image(self, class_value:str) -> bool:
-        class_value = class_value.strip()
-        if "img-thumbnail rounded m-0 p-0" in class_value:
-            return True
+        return url_category
+
+    def check_image(self, img_tag: Tag) -> bool:
+        if img_tag.has_attr("class"):
+            class_value = " ".join(img_tag["class"]).strip()
+            if "img-thumbnail rounded m-0 p-0" in class_value:
+                return True
+            else:
+                return False
         else:
             return False
+        
         
     def check_category(self, web_page) -> bool:
         # Make soup
         soup = BeautifulSoup(web_page.content, "html.parser", from_encoding="iso-8859-1")
 
+        pattern = re.compile(rf"{self.product_category}\s*-\s*{self.product_micro_category}", re.IGNORECASE)
+        
         for a_tag in soup.findAll("a", class_="p-0 m-0 text-decoration-none fs-4"):
             # Check if the a tag has only text as child and its value is the category of interest
-            if len(a_tag.findAll()) == 0 and self.product_category in a_tag.text.strip().lower():
-                return True
+            if a_tag.string:
+                tag_text = a_tag.get_text(strip= True).lower()
+
+                if pattern.search(tag_text):
+                    return True
         
         return False
