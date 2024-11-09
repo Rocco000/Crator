0d3371cbb42d9d0f7a6a21c5a3cf700aeb4ab469
Rocco000 <r.iuliano13@studenti.unisa.it>
2024-11-09 16:25:27 +0100

    Rocco000/Crator#1 Get base URL and get product category in the URL
    Developed two new methods: get_base_url() to get the base URL from the given URL; get_url_category() to extract the product category expressed in the URL

--- python/scrapers/scraper.py
+++ python/scrapers/scraper.py
@@ -1,9 +1,15 @@
 from abc import ABC, abstractmethod
+from bs4.element import Tag
 
 class Scraper(ABC):
 
-    def __init__(self, product_category):
+    def __init__(self, product_category: str, product_micro_category: str):
+        """
+        :param product_category: product category to be scraped.
+        :param product_micro_category: product micro-category to be scraped.
+        """
         self.product_category = product_category
+        self.product_micro_category = product_micro_category
 
     @abstractmethod
     def extract_product_information(self, web_page) -> dict:
@@ -15,10 +21,27 @@ class Scraper(ABC):
         pass
 
     @abstractmethod
-    def check_image(self, class_value:str) -> bool:
+    def get_base_url(self, web_page_url: str) -> str:
+        """
+        Extract the base URL from the given URL
+        :param web_page_url: the url of the visited web page
+        """
+        pass
+
+    @abstractmethod
+    def get_url_category(self, url: str) -> str:
+        """
+        Return the product category expressed in the URL
+        :param url: the url of the website being crawled
+        :return: product category expressed in the URL
+        """
+        pass
+
+    @abstractmethod
+    def check_image(self, img_tag: Tag) -> bool:
         """
         Checks the validity of the image's class attribute
-        :param class_value: value of the image's class attribute
+        :param img_tag: a <img> tag extracted from the current web page
         :return: True if the class attribute is correct, False otherwise
         """
         pass
